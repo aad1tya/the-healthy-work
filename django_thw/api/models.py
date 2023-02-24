cba3ff2d005c283f457b933bf9d59e5dd0551408
aad1tya <adityakumawat502@gmail.com>
2023-02-24 14:30:12 +0530

    Registered a model table and installed rest_framework in settings.py

--- django_thw/api/models.py
+++ django_thw/api/models.py
@@ -4,5 +4,10 @@ from django.db import models
 
 class Card(models.Model):
     #If the card body is empty don't show or create that card.
+    heading = models.CharField(max_length=300)
     bulletBody = models.TextField(null=False, blank=False)
+    created = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return "Card " + str(self.id) + ": "+ self.heading[0:50]
     
