bdc2405df8a302458c6977ef61a621c6b90c4dc6
aad1tya <adityakumawat502@gmail.com>
2023-02-27 12:19:47 +0530

    Added a main page component to the App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header'
+import CardsPage from './pages/CardsPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardsPage />
     </div>
   );
 }
---
+++ frontend/src/pages/CardsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardsPage = () => {
+  return (
+    <div>
+      This is the cards page where all the cards will be shown.
+    </div>
+  )
+}
+
+export default CardsPage
